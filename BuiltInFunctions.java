e75981294b73fa570acd129d5502501dd60c82d6
Rukayatu <rseidu941@gmail.com>
2026-06-05 16:07:53 +0200

    BuiltinFunction exercise completed

--- BuiltInFunctions.java
+++ BuiltInFunctions.java
@@ -1,8 +1,41 @@
 public class BuiltInFunctions {
     public static void main(String[] args) {
 
-        //Ejecuta aquí todos los métodos creados e imprime en la terminal. 
+     //Ejecuta aquí todos los métodos creados e imprime en la terminal. 
 
+     //Ejercicio 1
+     printMessage("Hello FemCoders!");
+
+     //Ejercicio 2
+     int length = getLength("FemCoder");
+     System.out.println("Length of String: " + length);
+        
+    //Ejercicio 3
+     String lowercase = convertToLowercase("JAVA PROGRAMMING");
+     System.out.println("Lowercase string: " + lowercase);
+
+    //Ejercicio 4
+     boolean startsWithPrefix = findPrefix("Unstoppable", "Un");
+     System.out.println("Starts with prefix: " + startsWithPrefix);
+
+     //Ejercicio 5
+    String replacedText = replaceCharacters("apple", 'p', 'm');
+    System.out.println("Replaced text: " + replacedText);
+
+     //Ejercicio 6
+     double squareRoot = getsSquareRoot(16.0);
+     System.out.println("Square root: " + squareRoot);
+     //Ejercicio 7
+     double powerResult = getPower(2.0, 3.0);
+     System.out.println("Power result: " + powerResult);
+              
+     //Ejercicio 8
+     double randomNumber = getRandomNumber();
+     System.out.println("Random number: " + randomNumber);
+
+    //Ejercicio 9
+    int max = maxNumber(45, 82);
+     System.out.println("Max number: " + max);
     }
 
         //Ejercicio 1:
@@ -16,7 +49,9 @@ public class BuiltInFunctions {
         */
 
         // Escribe tu código aquí
-        
+        public static void printMessage(String message) {
+          System.out.println(message);  
+        }
     
         //Ejercicio 2:
         /**
@@ -30,6 +65,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
+        public static int getLength(String message){
+          return message.length();  
+        }
 
 
         //Ejercicio 3:
@@ -44,7 +82,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-
+        public static String convertToLowercase(String message){
+        return message.toLowerCase();
+        }
 
         //Ejercicio 4:
         /**
@@ -59,7 +99,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-
+       public static boolean findPrefix(String message, String prefix) {
+            return message.startsWith(prefix);
+        }
 
         //Ejercicio 5:
         /**
@@ -75,7 +117,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-
+          public static String replaceCharacters(String message, char oldChar, char newChar) {
+            return message.replace(oldChar, newChar);
+        }
 
         //Ejercicio 6:
         /**
@@ -89,7 +133,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-
+         public static double getsSquareRoot(double number) {
+            return Math.sqrt(number);
+        }
 
         //Ejercicio 7:
         /**
@@ -104,7 +150,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-
+         public static double getPower(double base, double exponent) {
+            return Math.pow(base, exponent);
+        }
 
         //Ejercicio 8:
         /**
@@ -117,7 +165,9 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-
+        public static double getRandomNumber() {
+            return Math.random();
+        }
 
         //Ejercicio 9:
         /**
@@ -132,4 +182,7 @@ public class BuiltInFunctions {
          */
 
         // Escribe tu código aquí
-    }
+         public static int maxNumber(int number1, int number2) {
+            return Math.max(number1, number2);
+        }
+ }
